1b63045edf6bc00f9546a15f49c6779cc6dae1ad
jbudge3 <jake.budge@me.com>
2016-09-29 18:21:57 -0600

    Competed tasks

--- service.js
+++ service.js
@@ -1,10 +1,18 @@
-angular.module('userProfiles').service('mainService', function($http) {
+angular.module('userProfiles').service('mainService', function($http, $q) {
 
   this.getUsers = function() {
-    return $http({
+    var deffered = $q.defer();
+    $http({
         method: 'GET',
         url: 'http://reqres.in/api/users?page=1'
+    }).then(function(response) {
+      var parsedResponse = response.data.data;
+      for (var i = 0; i < parsedResponse.length; i++) {
+        parsedResponse[i].first_name = 'Ralph';
+      }
+      deffered.resolve(parsedResponse);
     })
+    return deffered.promise;
   }
 
 });
